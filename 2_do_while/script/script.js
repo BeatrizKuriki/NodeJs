a330bc09c5a0d98992fb277d3bfa99b76a97b158
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-15 20:01:42 -0300

    estrutura doWhile, while e for

---
+++ 2_do_while/script/script.js
@@ -0,0 +1,8 @@
+let x =100;
+
+
+do{
+    console.log(x/2);
+    x = x-5;
+
+}while(x > 0);
---
+++ 3_for/script/script.js
@@ -0,0 +1,5 @@
+
+
+for(let num = 0; num <100; num=num+2){
+    console.log(`A soma de num por 2 é igual: ${num +2}`);
+}
