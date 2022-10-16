3c7a2f5dc5644bd77e5bb7e1d60122c4a66253ff
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-16 00:30:33 -0300

    functions and arrow functions

--- funcao/script.js
+++ funcao/script.js
@@ -20,3 +20,39 @@ console.log(podeDirigir(15, 0));
 console.log(podeDirigir(25, 0));
 console.log(podeDirigir(30, 1));
 console.log(podeDirigir(18, false));
+
+//exemplo de escopo
+let n =10;
+
+function imprimir(){
+    let n = 150;
+    console.log(n);
+}
+
+imprimir();
+
+console.log(n);
+
+let consoleTeste = () => {
+    console.log("olá, mundo!");
+};
+
+consoleTeste();
+
+let soma = (a,b) => {
+    return a+b
+
+}
+
+console.log(soma(2,5));
+
+const multiplicarPorDois =(x) =>{
+    return x *2;
+}
+
+console.log(multiplicarPorDois(2));
+
+// fazendo a função mais enxuta
+
+const multiplicarPorDois2 = x => x*2;
+console.log(multiplicarPorDois2(5));
