9c6997d4ed800b8e7ccd9a715ef49ddaa276d6bd
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-15 21:49:45 -0300

    funcaoMath, If e Tipos variaveis

---
+++ exercicioEstruturaIf/script.js
@@ -0,0 +1,13 @@
+let idade = 18;
+
+if(idade >= 18){
+    console.log("Pode entrar!!")
+}else{
+    console.log("Não pode entrar você é menor de idade!");
+}
+
+const nome = "Beatriz";
+
+if(nome == "Beatriz"){
+    console.log("Olá, seja bem vinda,", nome)
+}
