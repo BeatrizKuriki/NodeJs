ec32113a392487042fd0f5b9e56d92c7aff8cfc0
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-15 21:58:59 -0300

    function math

---
+++ funcaoMath/script.js
@@ -0,0 +1,6 @@
+
+
+console.log (Math.pow(2,2));
+console.log (Math.pow(5,2));
+console.log (Math.pow(3,2));
+console.log (Math.pow(18,2));
---
+++ tiposVariaveis/script.js
@@ -0,0 +1,7 @@
+let nome = "Beatriz";
+let idade = 35;
+let casada = true;
+
+console.log(typeof(nome));
+console.log(typeof(idade));
+console.log(typeof(casada));
