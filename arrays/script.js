f5b3dbd66340a997d673830b5fc5051cf5eb4077
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-16 14:55:07 -0300

    métodos em arrays

--- arrays/script.js
+++ arrays/script.js
@@ -16,3 +16,25 @@ console.log(misturado);
  console.log(arr[arr.length-1]);
  //length - 2 pega o penúltimo e assim por diante 
  console.log(arr[arr.length-2]);
+
+ //PROPRIEDADES DOS ARRAYS
+
+console.log("Propriedades dos arrays");
+console.log("--------------------------------------")
+let firstName ='Beatriz';
+console.log(`O nome tem ${firstName.length} letras`);
+
+let numbers =[1,23,34,5,6,7,8,90];
+console.log(`O array tem ${numbers.length} elementos`);
+
+
+// MÉTODOS SÃO PROPRIEDADE QUE FUNCIONAM COMO FUNÇÕES
+//STRINGS E ARRAYS CONTÉM MÉTODOS
+
+let marca = "Nike";
+console.log(marca.toUpperCase());
+
+let marca2 = marca.toUpperCase();
+console.log(marca2.toLowerCase());
+
+console.log(typeof marca2.toLowerCase);
