9a992e38565c89e1ef1be9435e41ab66081589bf
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-16 14:35:34 -0300

    estudo arrays

---
+++ arrays/script.js
@@ -0,0 +1,18 @@
+let arr = [1,4,5,6,7];
+let nomes = ['Bia', 'Guilhereme', 'stella', 'igor'];
+let bool = [ true, false, true, true];
+let misturado = [1, 2, 'Bia', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
+
+//podemos acessar o array pelo seu índice
+
+ console.log(nomes[2]);
+ console.log (nomes[0]);
+ //length -1 pega o último elemento do array
+ console.log(arr[arr.length-1]);
+ //length - 2 pega o penúltimo e assim por diante 
+ console.log(arr[arr.length-2]);
